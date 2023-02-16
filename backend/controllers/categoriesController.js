3c65c286e686be8b9a84a9134dbda9e7ca60db88
augustinekirumba <steentoonz@gmail.com>
2023-02-16 11:59:55 +0300

    Updating the categories

--- backend/controllers/categoriesController.js
+++ backend/controllers/categoriesController.js
@@ -1,6 +1,20 @@
 const Category = require('../models/categoryModel')
 
 /* 
+    Method GET
+    Getting all categories
+*/
+const getCategories = (req, res) => {
+    Category.find()
+        .then((categories) => {
+            return res.status(200).json({ categories: categories });
+        })
+        .catch((err) => {
+            return res.status(500).json("There was an error when getting categories");
+        });
+}
+
+/* 
     Method POST
     Getting all categories
 */
@@ -32,20 +46,36 @@ const addRoute = (req, res, next) => {
 }
 
 /* 
-    Method GET
-    Getting all categories
+    Method PUT
+    Updating the categories
 */
-const getCategories = (req, res) => {
-    Category.find()
-        .then((categories) => {
-            return res.status(200).json({ categories: categories });
-        })
-        .catch((err) => {
-            return res.status(500).json("There was an error when getting categories");
-        });
+const putCategory = async(req, res) => {
+    // Validation... 
+    if (!req.body.category) {
+        return res.status(400).json({ error: "The category field cannot be empty" })
+    }
+
+    try {
+        const isCategoryId = await Category.exists({ _id: req.params.id })
+        if (isCategoryId) {
+            return res.status(400).json({ error: "Could not find a category with provided id" })
+        }
+    } catch (error) {
+        return res.status(400).json({ error: "Could not find a category with provided id" })
+    }
+    // Updating the category...
+    try {
+        const category = await Category.findById(req.params.id)
+        category.category = req.body.category
+        category.save()
+        res.status(200).json({ message: "The category was updated successifully" })
+    } catch (error) {
+        return res.status(500).json({ error: "Could not update the category" })
+    }
 }
 
 module.exports = {
     addRoute,
-    getCategories
+    getCategories,
+    putCategory
 }
--- backend/routes/categoryRoute.js
+++ backend/routes/categoryRoute.js
@@ -1,10 +1,11 @@
 const app = require('express');
-const { addRoute, getCategories } = require('../controllers/categoriesController');
+const { addRoute, getCategories, putCategory } = require('../controllers/categoriesController');
 
 const router = app.Router()
 
 // Handle routes...
 router.post("/", addRoute);
 router.get("/", getCategories);
+router.put("/:id", putCategory);
 
 module.exports = router;
